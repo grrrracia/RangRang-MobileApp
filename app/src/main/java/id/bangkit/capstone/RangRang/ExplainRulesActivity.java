4a471323bfccfeacf01e3ced16a3fe5042fac9b7
Gracia Angelica N <61301424+grrrracia@users.noreply.github.com>
2021-06-02 03:46:26 +0700

    Added Rules Page

---
+++ app/src/main/java/id/bangkit/capstone/RangRang/ExplainRulesActivity.java
@@ -0,0 +1,14 @@
+package id.bangkit.capstone.RangRang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExplainRulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explain_rules);
+    }
+}
