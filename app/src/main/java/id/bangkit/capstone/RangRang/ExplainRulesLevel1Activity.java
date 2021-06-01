7b890443fa1d1ca046c625a4d320693a5633b316
Gracia Angelica N <61301424+grrrracia@users.noreply.github.com>
2021-06-02 04:08:14 +0700

    Updated AndroidManifest for Camera Permissions

--- app/src/main/java/id/bangkit/capstone/RangRang/ExplainRulesLevel1Activity.java
+++ app/src/main/java/id/bangkit/capstone/RangRang/ExplainRulesLevel1Activity.java
@@ -2,6 +2,7 @@ package id.bangkit.capstone.RangRang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,9 @@ public class ExplainRulesLevel1Activity extends AppCompatActivity {
         btnStartLevel1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Mulai Level 1
+                //Start Level 1
+                Intent startLevel1 = new Intent(ExplainRulesLevel1Activity.this, Level1Activity.class);
+                startActivity(startLevel1);
             }
         });
     }
--- app/src/main/java/id/bangkit/capstone/RangRang/ExplainRulesLevel2Activity.java
+++ app/src/main/java/id/bangkit/capstone/RangRang/ExplainRulesLevel2Activity.java
@@ -2,6 +2,7 @@ package id.bangkit.capstone.RangRang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,9 @@ public class ExplainRulesLevel2Activity extends AppCompatActivity {
         btnStartLevel2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Mulai Level 2
+                //Start Level 2
+                Intent startLevel2 = new Intent(ExplainRulesLevel2Activity.this, Level2Activity.class);
+                startActivity(startLevel2);
             }
         });
     }
